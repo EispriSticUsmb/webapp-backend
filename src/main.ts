4461b558c5181c07c7c31c7c6fc28a13c74d2bb7
synnfall <synnfall@gmail.com>
2025-08-28 09:42:45 +0200

    ecoute toutes les ip qui lui sont attribué

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
-  await app.listen(Number(process.env.API_PORT ?? 3000));
+  const port = Number(process.env.API_PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
