08645ada408c2540c892a1c7bc5d47c2154c5758
synnfall <synnfall@gmail.com>
2025-10-19 21:49:30 +0200

    rendre facultatif des champs dans la modifications d'un utilisateur pour évtier les erreurs

--- src/share/dto/user.dto.ts
+++ src/share/dto/user.dto.ts
@@ -2,6 +2,7 @@ import { UserType } from '@prisma/client';
 import {
   IsEmail,
   IsNotEmpty,
+  IsOptional,
   IsString,
   Matches,
   MaxLength,
@@ -9,13 +10,14 @@ import {
 } from 'class-validator';
 
 export class PartialUserDto {
+  @IsOptional()
   @IsEmail({}, { message: 'Email invalide' })
   @MaxLength(64, {
     message: "L'Email doit contenir au plus 64 caractères",
   })
   email?: string;
 
-  @IsNotEmpty({ message: 'Username requis' })
+  @IsOptional()
   @MinLength(3, {
     message: "Le nom d'utilisateur doit contenir au moins 3 caractères",
   })
@@ -27,17 +29,20 @@ export class PartialUserDto {
   })
   username?: string;
 
+  @IsOptional()
   @IsString()
   @MaxLength(80, {
     message: 'Le prénom doit contenir au plus 80 caractères',
   })
   firstName?: string;
 
+  @IsOptional()
   @IsString()
   @MaxLength(100, {
     message: 'Le nom de famille doit contenir au plus 100 caractères',
   })
   lastName?: string;
 
+  @IsOptional()
   userType?: UserType;
 }
